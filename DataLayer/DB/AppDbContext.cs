99df77ca1fc463c8391384f75838a07c0c00ad0a
doshik <eurmonboyev@gmail.com>
2023-12-19 21:08:48 +0500

    дбни янгиси

--- DataLayer/DB/AppDbContext.cs
+++ DataLayer/DB/AppDbContext.cs
@@ -1,12 +1,32 @@
-﻿using System;
+﻿using DataAccessLayer.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection.Metadata;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace DataAccessLayer.DB;
 
-public class AppDbContext
+public class AppDbContext(DbContextOptions<AppDbContext> options) : DbContext(options)
 {
+    public DbSet<Answer> Answers { get; set; }
+    public DbSet<User> Users { get; set; }
+    public DbSet<Question> Question { get; set; }
+    public DbSet<Comment> Comments { get; set; }
+    public DbSet<Tag> Tags { get; set; }
+    public DbSet<QuestionTag> QuestionTags { get; set; }
+    public DbSet<Saved> Saves { get; set; }
 
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+
+        modelBuilder.Entity<QuestionTag>()
+        .HasMany(e => e.Questions)
+        .WithOne(e => e.QuestionTag)
+        .HasForeignKey(e => e.Id)
+        .IsRequired(false);
+    }
 }
