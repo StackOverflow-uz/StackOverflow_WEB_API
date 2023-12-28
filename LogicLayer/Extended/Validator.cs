c74917103c4d7a5daeb682a088416516c28759cb
Nurali <98206848+NuraliYuldashov@users.noreply.github.com>
2023-12-28 06:18:11 +0500

    tag user comment validators added

--- LogicLayer/Extended/Validator.cs
+++ LogicLayer/Extended/Validator.cs
@@ -12,5 +12,20 @@ public static class Validator
     public static bool IsValid(this Answer answer)
     => answer != null && !string.IsNullOrEmpty(answer.Body);
     public static bool IsExist(this Answer answer, IEnumerable<Answer> answers)
-        => answers.Any(c => c.Body == answer.Body && c.Id != answer.Id);
+        => answers.Any(c => c.Body == answer.Body && c.Id != answer.Id); 
+    
+    public static bool IsValid(this User answer)
+    => answer != null && !string.IsNullOrEmpty(answer.UserName);
+    public static bool IsExist(this User answer, IEnumerable<User> answers)
+        => answers.Any(c => c.UserName == answer.UserName && c.Id != answer.Id);
+
+    public static bool IsValid(this Comment answer)
+    => answer != null && !string.IsNullOrEmpty(answer.Text);
+    public static bool IsExist(this Comment answer, IEnumerable<Comment> answers)
+        => answers.Any(c => c.Text == answer.Text && c.Id != answer.Id);
+
+    public static bool IsValid(this Tag answer)
+    => answer != null && !string.IsNullOrEmpty(answer.Name);
+    public static bool IsExist(this Tag answer, IEnumerable<Tag> answers)
+        => answers.Any(c => c.Name == answer.Name && c.Id != answer.Id);
 }
